b7be0ef81a59a22c240d2a45c97b58da895953ca
João Paulo Menezes <joaopaulo.smenezes@hotmail.com>
2022-06-20 20:50:32 -0300

    update css and js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
 const drop = document.querySelectorAll(".drop");
+document.querySelector('.open-menu').addEventListener('click', openMenu);
+document.querySelector('.close-menu').addEventListener('click', closeMenu);
 
-document.querySelectorAll('.accordion-item').forEach(botao => botao.addEventListener('click', function(){    
-    
+function openMenu(){
+  document.body.classList.add('menu-expended');
+}
+function closeMenu(){
+  document.body.classList.remove('menu-expended');
+}
+
+
+document.querySelectorAll('.accordion-item').forEach(item => item.addEventListener('click', activeMenu)) ; 
+
+function activeMenu(){
     if(this.children[1].classList.contains("active")){
         this.children[1].classList.remove("active");
     }else{
-        drop.forEach(x => x.classList.remove('active'));
+        drop.forEach(element => element.classList.remove('active'));
         this.children[1].classList.add("active"); //children retorna os filhos de um elemento nesse caso pegamos apenas o filho [1]
-    }      
-}));
-/*
+    }   
+    /*
     childNodes - retorna uma NodeList dos elementos filhos.
     childElementCount - retorna a quantidade de filhos que aquele elemento possui
     children - retorna um vetor com os elementos filhos
 */
+}
+     
+
+
