01a875c7fd4b7c07c2696c84c8ae45dafbf086bf
João Paulo Menezes <joaopaulo.smenezes@hotmail.com>
2022-06-21 16:22:16 -0300

    accordion js final

--- js/script.js
+++ js/script.js
@@ -1,44 +1,39 @@
-const drop = document.querySelectorAll(".drop");
-// const arrowBaixo = document.querySelectorAll(".arrowbaixo");
-// const arrowCima = document.querySelectorAll(".arrowbaixo");
-
+const accordionItem = document.querySelectorAll('.accordion-item');
+accordionItem.forEach(item => item.addEventListener('click', activeMenu)) ;
 
 document.querySelector('.open-menu').addEventListener('click', openMenu);
 document.querySelector('.close-menu').addEventListener('click', closeMenu);
+
+
 function openMenu(){
   document.body.classList.add('menu-expended');
 }
 function closeMenu(){
   document.body.classList.remove('menu-expended');
 }
-
-document.querySelectorAll('.accordion-item').forEach(item => item.addEventListener('click', activeMenu)) ;
-
 function activeMenu(){    
-    if(this.children[1].classList.contains("active")){                
-        this.children[1].classList.remove("active");
-        // this.children[0].children[0].style.display = "inline-block";     
-        // this.children[0].children[1].style.display = "none";  
-    }else{
-        drop.forEach(element => element.classList.remove('active'));
-        this.children[1].classList.add("active"); //children retorna os filhos de um elemento nesse caso pegamos apenas o filho [1]         
-        // this.children[0].children[0].style.display = "none";     
-        // this.children[0].children[1].style.display = "inline-block";       
-    }       
-    
+    if(this.classList.contains("active")){                
+        this.classList.remove("active");       
+    }else{        
+        accordionItem.forEach(element => element.classList.remove('active'));
+        this.classList.add("active");           
+         
     /*
-    childNodes - retorna uma NodeList dos elementos filhos.
-    childElementCount - retorna a quantidade de filhos que aquele elemento possui
-    children - retorna um vetor com os elementos filhos    
-*/
-}
-// arrowRotate();
-// function arrowRotate(){    
-//     for(let i = 0; i < arrow.length; i++){
-//             if(arrow[i])
+        // this.children[1].classList.add("active");
+        //children retorna os filhos de um elemento nesse caso pegamos apenas o filho [1] 
         
-//     }
-// }
+        // this.children[1].children[2].classList.add("active");
+        //podemos pegar tbm o filho do filho, neste caso, estamos pegando o filho 2 do filho 1 de acordionItem
+
+        - childNodes - retorna uma NodeList dos elementos filhos.
+        - childElementCount - retorna a quantidade de filhos que aquele elemento possui
+        - children - retorna um vetor com os elementos filhos          
+        - previousElementSibling - propriedade retorna o elemento anterior no mesmo nível de árvore.
+        - nextElementSibling - retorna o próximo elemento no mesmo nível de árvore. 
+    */
+    }  
+}
+
      
 
 
